3eb8142e037e605580653aa2f7c118e306d9b119
mayconrcampos <maycon.campos@gmail.com>
2022-05-19 20:00:53 -0300

    criação de uma store que vai conter variáveis para o formulário

---
+++ src/store/formularioCadastroStore.js
@@ -0,0 +1,14 @@
+
+export default {
+    state: {
+        UFs: ["RO", "AC", "AM", "RR", "PA", "AP", "TO", "MA", "PI", "CE", "RN", "PB", "PE", "AL", "SE", "BA", "MG", "ES", "RJ", "SP", "PR", "SC", "RS", "MS", "MT", "GO", "DF"]
+    },
+
+    mutations: {
+        
+    },
+    actions: {
+        
+    },
+
+}
