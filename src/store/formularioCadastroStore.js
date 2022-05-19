94a12a9ef2a56fe15e912c1d5279a458441b2e6a
mayconrcampos <maycon.campos@gmail.com>
2022-05-19 20:02:04 -0300

    removido partes desnecessárias

--- src/store/formularioCadastroStore.js
+++ src/store/formularioCadastroStore.js
@@ -4,11 +4,4 @@ export default {
         UFs: ["RO", "AC", "AM", "RR", "PA", "AP", "TO", "MA", "PI", "CE", "RN", "PB", "PE", "AL", "SE", "BA", "MG", "ES", "RJ", "SP", "PR", "SC", "RS", "MS", "MT", "GO", "DF"]
     },
 
-    mutations: {
-        
-    },
-    actions: {
-        
-    },
-
 }
